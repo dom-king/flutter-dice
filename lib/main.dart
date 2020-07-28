2a70e07b2977650091ad6f06e4ad88322289d08a
Dom King <dminicking91@gmail.com>
2020-07-28 13:55:02 +0100

    Completed the dice randomizer application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Center(child: Text('Dicee')),
+          title: Center(child: Text('Dice')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,24 +16,38 @@ void main() {
   );
 }
 
-// hover over row and click flutter outline and you can center dice
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomizeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(child: FlatButton(
-            onPressed: () {
-              print('left button got pressed');
-            },
-            child: Image.asset('images/dice1.png')
+              onPressed: () {
+                 randomizeDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png')
           )),
           Expanded(child: FlatButton(
-            onPressed: () {
-              print('right button got pressed');
-            },
-            child: Image.asset('images/dice1.png')
+              onPressed: () {
+                randomizeDice();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png')
           )),
         ],
       ),
